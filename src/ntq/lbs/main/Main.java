209b781535d9eb6c2ec175fc3f8f8503262397a3
PhuongQuynh <tranductam2802@gmail.com>
2015-03-14 20:45:46 +0700

    Check

--- src/ntq/lbs/main/Main.java
+++ src/ntq/lbs/main/Main.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.swing.JFrame;
+
 import ntq.lbs.controller.ConfigReader;
 import ntq.lbs.controller.CrawlerQueue;
 import ntq.lbs.controller.CrawlerThread.IOnCrawVideo;
 import ntq.lbs.model.MySQLAccess;
 import ntq.lbs.model.MySQLAccess.IOnReadResult;
 import ntq.lbs.model.Video;
+import ntq.lbs.view.MainForm;
 
 public class Main {
 	private static int THREAHOLD = 39;
@@ -30,54 +33,15 @@ public class Main {
 		MySQLAccess dao = new MySQLAccess();
 		try {
 			// dao.delete("vpfn0UTNIx0");
-			CrawlerQueue.add("vpfn0UTNIx0", onCrawVideo);
-			CrawlerQueue.add("S-1peV-w0O0", onCrawVideo);
-			CrawlerQueue.add("yESQ4Fzq9_o", onCrawVideo);
-			CrawlerQueue.add("nZlTx9nxY-U", onCrawVideo);
-			CrawlerQueue.add("ZEcjXjpOo-Q", onCrawVideo);
-			CrawlerQueue.add("INrFJuLU0fM", onCrawVideo);
-			CrawlerQueue.add("wPrKAOpBeus", onCrawVideo);
-			CrawlerQueue.add("D-6JDufCJ1Y", onCrawVideo);
-			CrawlerQueue.add("X8mhF6HgzVA", onCrawVideo);
-			CrawlerQueue.add("x-UEksXSLGs", onCrawVideo);
-			CrawlerQueue.add("5iMx3tlpFeY", onCrawVideo);
-			CrawlerQueue.add("kvNEHnVSmvs", onCrawVideo);
-			CrawlerQueue.add("KE4I_W3jLuI", onCrawVideo);
-			CrawlerQueue.add("AdfSLq7XNoI", onCrawVideo);
-			CrawlerQueue.add("GefwYSJb_Bg", onCrawVideo);
-			CrawlerQueue.add("r1OtnOs-utU", onCrawVideo);
-			CrawlerQueue.add("BDw77bVNPo8", onCrawVideo);
-			CrawlerQueue.add("lAkoANzq750", onCrawVideo);
-			CrawlerQueue.add("lAkoANzq650", onCrawVideo);
-			CrawlerQueue.add("VGTQi5QqR0w", onCrawVideo);
-			CrawlerQueue.add("m6ndiBURmlQ", onCrawVideo);
-			CrawlerQueue.add("3jj-MSFwRfQ", onCrawVideo);
-			CrawlerQueue.add("RefgaMRfrG8", onCrawVideo);
-			CrawlerQueue.add("45LNKFmrViY", onCrawVideo);
-			CrawlerQueue.add("ZBT0Chsef0k", onCrawVideo);
-			CrawlerQueue.add("DKaZtnJLSZ0", onCrawVideo);
-			CrawlerQueue.add("rqxOucqbdyg", onCrawVideo);
-			CrawlerQueue.add("QETfX44-PB8", onCrawVideo);
-			CrawlerQueue.add("qbRIikllr-4", onCrawVideo);
-			CrawlerQueue.add("x3Xlqs1z_FI", onCrawVideo);
-			CrawlerQueue.add("tcat9CPiAZ4", onCrawVideo);
-			CrawlerQueue.add("i724lraI93s", onCrawVideo);
-			CrawlerQueue.add("V0e2cgpOrJQ", onCrawVideo);
-			CrawlerQueue.add("yESQ4Fzq9_o", onCrawVideo);
-			CrawlerQueue.add("xmfZ2U9bmZg", onCrawVideo);
-			CrawlerQueue.add("io-JPztHbhg", onCrawVideo);
-			CrawlerQueue.add("ZEcjXjpOo-Q", onCrawVideo);
-			CrawlerQueue.add("wjRgrezfccA", onCrawVideo);
-			CrawlerQueue.add("C2AiNyYiPDI", onCrawVideo);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		// Construct a new MainForm and show this form
-		// MainForm currentMainForm = MainForm.getInstance();
-		// currentMainForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// currentMainForm.setVisible(true);
+		MainForm currentMainForm = MainForm.getInstance();
+		currentMainForm.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		currentMainForm.setVisible(true);
 	}
 
 	private static IOnCrawVideo onCrawVideo = new IOnCrawVideo() {
